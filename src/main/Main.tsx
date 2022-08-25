1a36964ccae44b171bfa30a9b719c25419fb9760
Andrewwxo <andrewwxo@gmail.com>
2022-08-25 19:07:32 +0300

    1st commit

---
+++ src/main/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Main.module.css';
+import styleContainer from '../../src/common/styles/Container.module.css'
+
+export function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={styleContainer.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>Andrei Novitskii</h1>
+                    <p>Fronted Developer</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+
+        </div>
+    );
+}
+
---
+++ src/skills/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Skills.module.css';
+import styleContainer from '../../src/common/styles/Container.module.css'
+
+export function Skills() {
+    return (
+        <div className={style.skillsBlock}>
+            <div className={styleContainer.container}></div>
+        </div>
+    )
+}
